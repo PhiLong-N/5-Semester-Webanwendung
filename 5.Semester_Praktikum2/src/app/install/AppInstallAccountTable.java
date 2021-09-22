8041f86a78b127b3aee98c7ddbcf4cb530ac74f7
Phi Long <Phi Long@10.8.4.18>
2021-09-23 01:19:06 +0200

    test

--- 5.Semester_Praktikum2/src/app/install/AppInstallAccountTable.java
+++ 5.Semester_Praktikum2/src/app/install/AppInstallAccountTable.java
@@ -26,9 +26,6 @@ public class AppInstallAccountTable {
 		insertAdmin();
 		insertKunde();
 		
-		//maxClicks();
-		//update();
-		//force();
 	}
 	
 	
@@ -71,33 +68,6 @@ public class AppInstallAccountTable {
 		System.out.println("Gast hinzugefügt");
 	}
 	
-	
-	
-	
-	public void maxClicks() throws NoConnectionException, SQLException {
-		String sql="select max(clicks) as clicks from artikel";
-		ResultSet dbRes = new PostgreSQLAccess().getConnection().prepareStatement(sql).executeQuery();
-		dbRes.next();
-		int clicks =dbRes.getInt("clicks");
-		sql = "select artikelnr, artikel from artikel where clicks ="+clicks;
-		dbRes = new PostgreSQLAccess().getConnection().prepareStatement(sql).executeQuery();
-		dbRes.next();
-		String artikel = dbRes.getString("artikel");
-		int artikelnr = dbRes.getInt("artikelnr");
-	}
-	
-	public void update() throws SQLException{
-		//String sql = "UPDATE account set password = 'bob' where accnr = 1";
-		String sql ="UPDATE account set lastvisit='http://localhost:8080/A_Praktikum_test/jsp/ArtikelSeiteView.jsp?btnArtikel=6' where accnr =1";
-		PreparedStatement prep = this.dbConn.prepareStatement(sql);
-		prep.executeUpdate();
-		System.out.println("Gast hinzugefügt");
-	}
-	
-	public void force() throws SQLException {
-		String sql = "update account set lastvisitint =0 where accnr= 2";
-		PreparedStatement prep = this.dbConn.prepareStatement(sql);
-		prep.executeUpdate();
-	}
+
 	
 }
